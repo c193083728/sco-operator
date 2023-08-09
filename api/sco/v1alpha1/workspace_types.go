0b04852aa83a3986b0f6039562dfbb1bc01aa629
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-08-09 16:32:47 +0200

    intial scaffold

---
+++ api/sco/v1alpha1/workspace_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type WorkspaceSpec struct {
+}
+
+type WorkspaceStatus struct {
+	Phase              string             `json:"phase"`
+	Conditions         []metav1.Condition `json:"conditions,omitempty"`
+	ObservedGeneration int64              `json:"observedGeneration,omitempty"`
+	Endpoint           string             `json:"endpoint,omitempty"`
+}
+
+// +genclient
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`,description="The phase"
+// +kubebuilder:resource:path=workspaces,scope=Namespaced,shortName=ws,categories=integration;camel
+
+type Workspace struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WorkspaceSpec   `json:"spec,omitempty"`
+	Status WorkspaceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+type WorkspaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Workspace `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Workspace{}, &WorkspaceList{})
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
+
+	"github.com/c193083728/sco-operator/cmd/run"
+	"github.com/c193083728/sco-operator/pkg/logger"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "sco",
+		Short: "sco",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	rootCmd.AddCommand(run.NewRunCmd())
+
+	fs := flag.NewFlagSet("", flag.PanicOnError)
+
+	klog.InitFlags(fs)
+	logger.Options.BindFlags(fs)
+
+	rootCmd.PersistentFlags().AddGoFlagSet(fs)
+
+	if err := rootCmd.Execute(); err != nil {
+		klog.ErrorS(err, "problem running command")
+		os.Exit(1)
+	}
+}
