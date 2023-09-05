8a39392601e32d39032f6cbf1ef8c764e8313eb8
Luis Garcia Acosta <lgarciaa@redhat.com>
2023-09-05 11:22:51 +0200

    [chore] create camel-k dependency
    Create bundle camel-k dependency to force camel-k installation if not already installed.
    
    Signed-off-by: Luis Garcia Acosta <lgarciaa@redhat.com>

--- hack/scripts/gen_bundle.sh
+++ hack/scripts/gen_bundle.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
-    echo "project root, bundle name, bundle version are expected"
+if [ $# -ne 4 ]; then
+    echo "project root, bundle name, bundle version and camelk version are expected"
 fi
 
 PROJECT_ROOT="$1"
 BUNDLE_NAME="$2"
 BUNDLE_VERSION="$3"
+DEPENDENCY_CAMELK_VERSION="$4"
 
 rm -rf "${PROJECT_ROOT}/bundle/${BUNDLE_NAME}"
 
@@ -28,11 +29,22 @@ ${PROJECT_ROOT}/bin/kustomize build "${PROJECT_ROOT}/config/manifests" | ${PROJE
   --default-channel "alpha" \
   --output-dir "${BUNDLE_NAME}"
 
+cat > "${BUNDLE_NAME}/metadata/dependencies.yaml" <<'_EOF'
+dependencies:
+  - type: olm.package
+    value:
+      packageName: camel-k
+      version: "DEPENDENCY_CAMELK_VERSION"
+_EOF
+
 echo "Patch bundle metadata"
 
 ${PROJECT_ROOT}/bin/yq -i \
   '.metadata.annotations.containerImage = .spec.install.spec.deployments[0].spec.template.spec.containers[0].image' \
    "${PROJECT_ROOT}/bundle/${BUNDLE_NAME}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
+
+sed -i s/DEPENDENCY_CAMELK_VERSION/"${DEPENDENCY_CAMELK_VERSION}"/g \
+  "${PROJECT_ROOT}/bundle/${BUNDLE_NAME}/metadata/dependencies.yaml"
 
 echo "Validate bundle"
 
