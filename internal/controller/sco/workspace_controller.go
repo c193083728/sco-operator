86790c99fb07043c156e1c693e6da89a8d41565c
LuLo <github@had3s.one>
2023-09-14 12:06:08 +0200

    [chore] add deploy action to workspace api (#17)
    * [chore] add deploy action to workspace api
    
    Add deploy action. Manage camelk resources
    
    ---------
    
    Signed-off-by: Luis Garcia Acosta <lgarciaa@redhat.com>

--- internal/controller/sco/workspace_controller.go
+++ internal/controller/sco/workspace_controller.go
@@ -18,6 +18,14 @@ package sco
 
 import (
 	"context"
+	"sort"
+
+	wsApi "github.com/sco1237896/sco-operator/api/sco/v1alpha1"
+	"go.uber.org/multierr"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/sco1237896/sco-operator/pkg/controller"
 
@@ -28,9 +36,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	wsApi "github.com/sco1237896/sco-operator/api/sco/v1alpha1"
 )
 
 func NewKWorkspaceReconciler(manager ctrl.Manager) (*WorkspaceReconciler, error) {
@@ -39,11 +44,14 @@ func NewKWorkspaceReconciler(manager ctrl.Manager) (*WorkspaceReconciler, error)
 		return nil, err
 	}
 
-	rec := WorkspaceReconciler{}
-	rec.l = ctrl.Log.WithName("controller")
-	rec.Client = c
-	rec.Scheme = manager.GetScheme()
-	rec.ClusterType = controller.ClusterTypeVanilla
+	rec := WorkspaceReconciler{
+		Client:      c,
+		Scheme:      manager.GetScheme(),
+		ClusterType: controller.ClusterTypeVanilla,
+		actions:     make([]controller.Action[wsApi.Workspace], 0),
+		l:           ctrl.Log.WithName("controller"),
+	}
+	rec.actions = append(rec.actions, NewDeployAction(rec.l))
 
 	isOpenshift, err := c.IsOpenShift()
 	if err != nil {
@@ -52,8 +60,6 @@ func NewKWorkspaceReconciler(manager ctrl.Manager) (*WorkspaceReconciler, error)
 	if isOpenshift {
 		rec.ClusterType = controller.ClusterTypeOpenShift
 	}
-
-	rec.actions = make([]controller.Action[wsApi.Workspace], 0)
 
 	return &rec, nil
 }
@@ -80,17 +86,73 @@ type WorkspaceReconciler struct {
 // +kubebuilder:rbac:groups="route.openshift.io",resources=routes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="networking.k8s.io",resources=ingresses,verbs=get;list;watch;create;update;patch;delete
 
-func (r *WorkspaceReconciler) Reconcile(ctx context.Context, _ ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *WorkspaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.l.Info("Reconciling", "resource", req.NamespacedName.String())
 
-	// TODO(user): your logic here
+	rr := controller.ReconciliationRequest[wsApi.Workspace]{
+		Client: r.Client,
+		NamespacedName: types.NamespacedName{
+			Name:      req.Name,
+			Namespace: req.Namespace,
+		},
+		ClusterType: r.ClusterType,
+		Resource:    &wsApi.Workspace{},
+	}
 
-	return ctrl.Result{}, nil
+	err := r.Get(ctx, req.NamespacedName, rr.Resource)
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			// no CR found
+			return ctrl.Result{}, nil
+		}
+	}
+
+	reconcileCondition := metav1.Condition{
+		Type:               "Reconcile",
+		Status:             metav1.ConditionTrue,
+		Reason:             "Reconciled",
+		Message:            "Reconciled",
+		ObservedGeneration: rr.Resource.Generation,
+	}
+	var allErrors error
+
+	for i := range r.actions {
+		if err := r.actions[i].Apply(ctx, &rr); err != nil {
+			allErrors = multierr.Append(allErrors, err)
+		}
+	}
+
+	if allErrors != nil {
+		reconcileCondition.Status = metav1.ConditionFalse
+		reconcileCondition.Reason = "Failure"
+		reconcileCondition.Message = "Failure"
+
+		rr.Resource.Status.Phase = "Error"
+	} else {
+		rr.Resource.Status.ObservedGeneration = rr.Resource.Generation
+		rr.Resource.Status.Phase = "Ready"
+	}
+
+	meta.SetStatusCondition(&rr.Resource.Status.Conditions, reconcileCondition)
+
+	sort.SliceStable(rr.Resource.Status.Conditions, func(i, j int) bool {
+		return rr.Resource.Status.Conditions[i].Type < rr.Resource.Status.Conditions[j].Type
+	})
+
+	// Update status
+	err = r.Status().Update(ctx, rr.Resource)
+	if err != nil && k8serrors.IsConflict(err) {
+		r.l.Info(err.Error())
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		allErrors = multierr.Append(allErrors, err)
+	}
+
+	return ctrl.Result{}, allErrors
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *WorkspaceReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
-
 	c := ctrl.NewControllerManagedBy(mgr)
 
 	c = c.For(&wsApi.Workspace{}, builder.WithPredicates(
